6c656b2c6e40b5ff7171b93b2fb3c4b89d05fd05
pib319 <pib31995@yahoo.com>
2018-10-27 19:32:44 -0700

    Created Main Menu UI
    This also added some assets as well. Still working on getting the Main Menu to look correct.

---
+++ Tower of Valor/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene1 ()
+    {
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
