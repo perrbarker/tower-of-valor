d548287e81757902f1fd1c88e8cb56d8b9f38473
pib319 <pib31995@yahoo.com>
2018-11-04 19:12:37 -0800

    Added Platforms
    Thin and Thick platforms have been created. They still need sprite artwork.

---
+++ Tower of Valor/Assets/Scripts/OneWayPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            if (waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+                effector.rotationalOffset = 0f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+}
