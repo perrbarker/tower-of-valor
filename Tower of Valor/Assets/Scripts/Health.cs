c4ac0c50a57a7129c02d57cb40a961f59db6e4a8
perrbear17 <perrb303@coyote.csusb.edu>
2018-11-17 21:36:54 -0800

    Respawn with Lives
    Respawn works but I am looking into Setting up multiple spawns and the closest spawn being what is used.

--- Tower of Valor/Assets/Scripts/Health.cs
+++ Tower of Valor/Assets/Scripts/Health.cs
@@ -5,7 +5,9 @@ public class Health : MonoBehaviour {
 
     public int maxHP;
     public int hitPoints;
+	public int lives;
     public float timeToDestroy;
+	public GameObject spawn;
 
     void Start()
     {
@@ -23,6 +25,14 @@ public class Health : MonoBehaviour {
     public int HitPoints
     {
         get { return hitPoints; }
+		set
+		{
+			hitPoints += value;
+			if (hitPoints > maxHP)
+			{
+				hitPoints = maxHP;
+			}
+		}
 
     }
 
@@ -35,15 +45,43 @@ public class Health : MonoBehaviour {
     IEnumerator Death()
     {
         yield return new WaitForSeconds(timeToDestroy);
-		if (gameObject.transform == Camera.main.GetComponent<CameraScript> ().player1)
-		{
-			Camera.main.GetComponent<CameraScript> ().SetPlayer1 = Camera.main.GetComponent<CameraScript> ().player2;
 
-		}
-		else if (gameObject.transform == Camera.main.GetComponent<CameraScript> ().player2)
+		if (gameObject.tag == "Player")
 		{
-			Camera.main.GetComponent<CameraScript> ().SetPlayer2 = Camera.main.GetComponent<CameraScript> ().player1;
+			hitPoints = maxHP;
+			--lives;
+			if (lives < 0)
+			{
+				Destroy (gameObject);
+
+				if (gameObject.transform == Camera.main.GetComponent<CameraScript> ().player1)
+				{
+					Camera.main.GetComponent<CameraScript> ().SetPlayer1 = Camera.main.GetComponent<CameraScript> ().player2;
+
+				}
+				else if (gameObject.transform == Camera.main.GetComponent<CameraScript> ().player2)
+				{
+					Camera.main.GetComponent<CameraScript> ().SetPlayer2 = Camera.main.GetComponent<CameraScript> ().player1;
+				}
+			}
+			else
+			{
+				Spawn respawn = spawn.GetComponent<Spawn> ();
+				respawn.Respawn ();
+			}
 		}
-		Destroy (gameObject);
+		else
+		{
+			Destroy(gameObject
+		}
     }
+
+	public GameObject Spawn
+	{
+		get { return spawn; }
+		set
+		{
+			spawn = value;
+		}
+	}
 }
