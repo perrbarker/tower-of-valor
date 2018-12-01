019c725b6f9c0f953ad4272faaba21ce64d92531
Jeremy Noble <005940658@coyote.csusb.edu>
2018-12-01 10:38:43 -0800

    readjusted background tiles and script

--- Tower of Valor/Assets/Scripts/Leap_Frog.cs
+++ Tower of Valor/Assets/Scripts/Leap_Frog.cs
@@ -6,45 +6,36 @@ public class Leap_Frog : MonoBehaviour {
 
 	public Transform tile_1, tile_2, camPosy; // position of tile
 	public Vector3 offset; // starting position of each tile
-	public float Y_MOVE;
-	public float OVERLAP; // distance to move each tile
-	public float t1Pos, t2Pos;
+	private const float Y_MOVE = 43.2f, OVERLAP = 23; // distance to move each tile
+	private float t1Pos, t2Pos;
 
-	// Use this for initialization
-	void Start () {
-		//tile_1.transform.position = offset;
-		//tile_2.transform.position = new Vector3(0f, X_SCALE, 0f);
+	void Start() {
 		t1Pos = tile_1.position.y;
 		t2Pos = tile_2.position.y;
-	}
+		}
 	
 	// update the position of the background tiles in relation to the camera
 	void Update () {
 
 		// camera is above tile 1, player is in front of tile 2
-		if (camPosy.transform.position.y >= tile_1.transform.position.y + OVERLAP) {
-			Debug.Log ("Moving Background: tile1 up");
+		if (camPosy.transform.position.y > tile_1.transform.position.y + OVERLAP) {
 			t1Pos += Y_MOVE;
 			tile_1.transform.position = new Vector3(0, t1Pos, 0);
 		}
 		// camera is above tile 2, player is in front of tile 1
-		else if (camPosy.transform.position.y >= tile_2.transform.position.y + OVERLAP) {
-			Debug.Log ("Moving Background: tile2 up");
+		else if (camPosy.transform.position.y > tile_2.transform.position.y + OVERLAP) {
 			t2Pos += Y_MOVE;
 			tile_2.transform.position = new Vector3(0, t2Pos, 0);
 		}
 		// camera is below tile 1, player is in front of tile 2
-		else if (camPosy.transform.position.y < tile_2.transform.position.y - OVERLAP/2 && t1Pos > t2Pos) {
-			Debug.Log ("Moving Background: tile1 down");
+		else if (camPosy.transform.position.y < tile_1.transform.position.y - OVERLAP) {
 			t1Pos -= Y_MOVE;
 			tile_1.transform.position = new Vector3(0, t1Pos, 0);
 		}
-		// camera is below tile 1, and tile 1 is the lower
-		else if (camPosy.transform.position.y < tile_1.transform.position.y - OVERLAP/2 && t2Pos > t1Pos) {
-			Debug.Log ("Moving Background: tile2 down");
+		// camera is below tile 2, plater is in front of tile 1
+		else if (camPosy.transform.position.y < tile_2.transform.position.y - OVERLAP) {
 			t2Pos -= Y_MOVE;
 			tile_2.transform.position = new Vector3(0, t2Pos, 0);
 		}
-
 	}
 }
