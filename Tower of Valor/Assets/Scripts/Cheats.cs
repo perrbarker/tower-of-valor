cd9fc41153073ef7d819a69818822de883f18387
Jameson Dy <dyjameson22@gmail.com>
2018-12-03 00:11:14 -0800

    Added Wizard on top of level in P2 scene

---
+++ Tower of Valor/Assets/Scripts/Cheats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Cheats : MonoBehaviour {
+
+    public Transform p1;
+    public KeyCode teleport;
+    public Transform telPosition;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(teleport))
+        {
+            p1.position = telPosition.position;
+        }
+
+    }
+}
